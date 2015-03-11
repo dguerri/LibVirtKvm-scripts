32f0b5f7bd74bda3b58abe04c9097cde1e7a74a0
AJRepo <AJRepo@users.noreply.github.com>
2015-03-11 09:00:46 -0500

    Add -q flag to not get headers

--- fi-backup.sh
+++ fi-backup.sh
@@ -213,7 +213,7 @@ function consolidate_domain() {
    local command_output=
    local parent_backing_file=
 
-   local block_devices=$($VIRSH -r domblklist "$domain_name" | awk '/^vd[a-z][[:space:]]+/ {print $2}')
+   local block_devices=$($VIRSH -q -r domblklist "$domain_name" | awk '/^vd[a-z][[:space:]]+/ {print $2}')
    _ret=$?
    if [ $_ret -ne 0 ]; then
       print_v e "Error getting block device list for domain '$domain_name'"
